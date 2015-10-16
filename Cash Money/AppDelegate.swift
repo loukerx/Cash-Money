d770e5cf48ead30d1e881cf030d59c6f2d17d8a7
Yin Hua <loukerx@gmail.com>
2015-10-16 18:04:35 +1100

    scrollview

--- Cash Money/AppDelegate.swift
+++ Cash Money/AppDelegate.swift
@@ -8,14 +8,27 @@
 
 import UIKit
 
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var appHelper = AppHelper()
+    
+    //color
+    var appBackgroundColor:UIColor?
+    var scrollViewBackgrundColor:UIColor?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
+        //color
+        appBackgroundColor = appHelper.colorWithHexString("40C866")//40C866
+        scrollViewBackgrundColor = appHelper.colorWithHexString("3FB760")
+        
+        
+        
         return true
     }
 
--- Cash Money/ViewController.swift
+++ Cash Money/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
     //MARK: Outlets
     @IBOutlet var mainView: UIView!
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var AUDTextField: UITextField!
     
+    @IBOutlet weak var dashBorderView: UIView!
+    
     @IBOutlet weak var scrollView: UIScrollView!
     
     
@@ -24,6 +26,11 @@ class ViewController: UIViewController {
     
     //variable
     var appDelegate = AppDelegate()
+    let currencyArray = ["CAD","EUR","GBP","JPY","USD"]
+    let widthRatio:CGFloat = 0.4
+    
+    
+    
     
     //MARK: - ViewDidLoad
     override func viewDidLoad() {
@@ -31,6 +38,8 @@ class ViewController: UIViewController {
 
         appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
 
+        self.settingView()
+        self.prepareScrollView()
         
         
         
@@ -38,11 +47,86 @@ class ViewController: UIViewController {
 
     private func settingView(){
         
-        self.mainView.backgroundColor = UIColor.greenColor()
-        self.upView.backgroundColor = UIColor.clearColor()
+        self.mainView.backgroundColor = appDelegate.appBackgroundColor
+        self.scrollView.backgroundColor = appDelegate.scrollViewBackgrundColor
+//        self.upView.backgroundColor = UIColor.clearColor()
+        
+       /*
+        //Dash Border
+        let borderLayer = CAShapeLayer()
+        let viewWidth = self.dashBorderView.bounds.size.width
+        let viewHeight = self.dashBorderView.bounds.size.height
+        borderLayer.bounds = CGRectMake(0, 0, viewWidth, viewHeight)
+        borderLayer.position = CGPointMake(CGRectGetMaxX(self.dashBorderView.bounds), CGRectGetMaxY(self.dashBorderView.bounds))
+        borderLayer.path = UIBezierPath.init(roundedRect: borderLayer.bounds, cornerRadius: CGRectGetWidth(borderLayer.bounds)/2).CGPath
+        borderLayer.lineWidth = 1
+        
+        borderLayer.lineDashPattern = [8,8]
+        borderLayer.fillColor = UIColor.clearColor().CGColor
+        borderLayer.strokeColor = UIColor.blackColor().CGColor
+        
+        self.dashBorderView.layer.addSublayer(borderLayer)
+        self.dashBorderView.backgroundColor = UIColor.clearColor()
+        
+        
+*/
+        
+
         
     }
+    
+    private func prepareScrollView(){
+        
+        let width = self.view.bounds.width * widthRatio
+        let height = self.view.bounds.height
+        let currencyCount = self.currencyArray.count
+        let contentSizeWidth = width * CGFloat(currencyCount)
+        self.scrollView.contentSize = CGSizeMake(contentSizeWidth, 0)
+        self.scrollView.delegate = self
+        
+        var count:Int = 0
+        
+        for name in self.currencyArray {
+            
+            
+            let nameStr = name as String
+            
+            let currencyLabel = UILabel(frame: CGRectMake(width * CGFloat(count), 0, width, height))
+            currencyLabel.text = nameStr
+            currencyLabel.textColor = UIColor.whiteColor()
+//            currencyLabel.backgroundColor = UIColor.clearColor()
+            currencyLabel.textAlignment = NSTextAlignment.Center
+            currencyLabel.font = UIFont(name: "System", size: 30)
+            
+                
+            
+            self.scrollView.addSubview(currencyLabel)
+            
+            
+   
+            count++
+            
+            
+        }
+        
+        
+        
+        
+    }
+    
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
