c9bf2ab4552e0f7be6406936c0e7f3c128a65d22
Yin Hua <loukerx@gmail.com>
2015-10-18 00:35:26 +1100

    autolayout font size draw dash line

---
+++ Cash Money/DashLine.swift
@@ -0,0 +1,31 @@
+//
+//  DashLine.swift
+//  Cash Money
+//
+//  Created by Yin Hua on 17/10/2015.
+//  Copyright © 2015 Yin Hua. All rights reserved.
+//
+
+import UIKit
+
+class DashLine: UIView {
+
+
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+        let context = UIGraphicsGetCurrentContext()
+        CGContextSetLineWidth(context, 4.0)
+        CGContextSetStrokeColorWithColor(context, UIColor.blackColor().CGColor)
+        
+        let dashArray:[CGFloat] = [7,7]
+        CGContextSetLineDash(context, 10, dashArray, 4)
+        
+        CGContextMoveToPoint(context, 0, 0)
+        CGContextAddLineToPoint(context, self.frame.size.width, 0)
+        CGContextStrokePath(context)
+        
+    }
+    
+}
--- Cash Money/ViewController.swift
+++ Cash Money/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import Alamofire
+//import Alamofire
 
 class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegate, NSURLConnectionDataDelegate {
 
@@ -41,11 +41,8 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
 
         
         self.settingView()
-
-//        self.prepareData()//修改普通读取jason的方法
         self.propareCurrencyArray()
-        //判断金额长度
-        //draw dash
+        
         //修改字体  The font is Helvetica (56pt max, scaling down dynamically as required eg. for really big numbers).
 
         
@@ -54,6 +51,14 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
 
     }
 
+    override func viewDidLayoutSubviews() {
+        if let pointSize = self.AUDTextField.font?.pointSize{
+            print(pointSize)
+            self.amountDisplayLabel.font = UIFont(name: "Helvetica", size: pointSize)
+            
+        }
+    }
+    
     override func viewDidAppear(animated: Bool) {
         super.viewWillAppear(true)
         
@@ -61,19 +66,34 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
         self.prepareScrollView()
     }
     
+    private func settingView(){
+        
+        self.mainView.backgroundColor = appDelegate.appBackgroundColor
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "DismissKeyboard")
+        self.view.addGestureRecognizer(tap)
+        
+    }
+    
+    
+    func DismissKeyboard(){
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        self.view.endEditing(true)
+    }
     
     private func prepareScrollView(){
         
         //修改paging size，可视size不一样
         
-        
-        let width = self.scrollView.frame.size.width// * widthRatio
+        let width = self.scrollView.frame.size.width * 0.5// * widthRatio
         let height = self.scrollView.frame.size.height
         let currencyCount = self.currencyArray.count
         let contentSizeWidth = width * CGFloat(currencyCount)
         self.scrollView.contentSize = CGSizeMake(contentSizeWidth, 0)
         self.scrollView.delegate = self
-        self.scrollView.backgroundColor = UIColor.clearColor()
+        self.scrollView.backgroundColor = appDelegate.scrollViewBackgrundColor
+//        self.scrollView.pagingEnabled = true
+
         
         var count:Int = 0
         
@@ -85,19 +105,16 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
             let currencyLabel = UILabel(frame: CGRectMake(width * CGFloat(count), 0, width, height))
             currencyLabel.text = nameStr
             currencyLabel.textColor = UIColor.whiteColor()
-            //            currencyLabel.backgroundColor = UIColor.clearColor()
             currencyLabel.textAlignment = NSTextAlignment.Center
-            currencyLabel.font = UIFont(name: "Helvetica", size: 50)
-//            currencyLabel.backgroundColor = UIColor.blackColor()
+            currencyLabel.font = UIFont(name: "Helvetica", size: 56)
+            currencyLabel.backgroundColor = UIColor.clearColor()
+            
             if count != 0{
                 currencyLabel.alpha = self.unselectedLabelAlpha
             }
-            currencyLabel.layer.borderColor = UIColor.whiteColor().CGColor
-            currencyLabel.layer.borderWidth = 1.0
-
-
-            //            currencyLabel.layer.cornerRadius = 4.0
-            currencyLabel.clipsToBounds = true
+//            currencyLabel.layer.borderColor = UIColor.whiteColor().CGColor
+//            currencyLabel.layer.borderWidth = 1.0
+//            currencyLabel.clipsToBounds = true
             
             self.scrollView.addSubview(currencyLabel)
             
@@ -115,8 +132,8 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
     
     //MARK: ScrollView Delegate
     func scrollViewDidScroll(scrollView: UIScrollView) {
-        let pageWidth = scrollView.frame.size.width
-        let page = Int((self.scrollView.contentOffset.x * 2.0 + pageWidth) / CGFloat(pageWidth * 2.0))
+        let pageWidth = scrollView.frame.size.width * 0.5
+        let page = Int((self.scrollView.contentOffset.x * 2.0 * 0.5 + pageWidth) / CGFloat(pageWidth * 2.0))
         
         self.selectedCurrencyTag = page
         
@@ -141,52 +158,11 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
                     
                     currencyLabel.alpha = 1
                 }else{
-                    currencyLabel.alpha = 0.3
+                    currencyLabel.alpha = self.unselectedLabelAlpha
                 }
             }
         }
     }
-    
-    private func settingView(){
-        
-        self.mainView.backgroundColor = appDelegate.appBackgroundColor
-        self.scrollView.backgroundColor = appDelegate.scrollViewBackgrundColor
-//        self.upView.backgroundColor = UIColor.clearColor()
-        
-       /*
-        //Dash Border
-        let borderLayer = CAShapeLayer()
-        let viewWidth = self.dashBorderView.bounds.size.width
-        let viewHeight = self.dashBorderView.bounds.size.height
-        borderLayer.bounds = CGRectMake(0, 0, viewWidth, viewHeight)
-        borderLayer.position = CGPointMake(CGRectGetMaxX(self.dashBorderView.bounds), CGRectGetMaxY(self.dashBorderView.bounds))
-        borderLayer.path = UIBezierPath.init(roundedRect: borderLayer.bounds, cornerRadius: CGRectGetWidth(borderLayer.bounds)/2).CGPath
-        borderLayer.lineWidth = 1
-        
-        borderLayer.lineDashPattern = [8,8]
-        borderLayer.fillColor = UIColor.clearColor().CGColor
-        borderLayer.strokeColor = UIColor.blackColor().CGColor
-        
-        self.dashBorderView.layer.addSublayer(borderLayer)
-        self.dashBorderView.backgroundColor = UIColor.clearColor()
-        
-        
-*/
-        
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "DismissKeyboard")
-        self.view.addGestureRecognizer(tap)
-        
-    }
-    
-    
-    func DismissKeyboard(){
-        //Causes the view (or one of its embedded text fields) to resign the first responder status.
-        self.view.endEditing(true)
-    }
-
-  
-    
-    
     
     
     //MARK: - Prepare Data
@@ -216,9 +192,6 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
         
         let url = NSURL(string: "http://api.fixer.io/latest?base=AUD&symbols=CAD,EUR,GBP,JPY,USD")
         let request = NSURLRequest(URL: url!)
-//        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
-//            print(NSString(data: data!, encoding: NSUTF8StringEncoding))
-//        }
         _ = NSURLConnection(request: request, delegate:self, startImmediately: true)
 
     }
@@ -259,22 +232,15 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
     func textFieldDidBeginEditing(textField: UITextField) {
         if textField.tag == audTextFieldTag && textField.text?.characters.count>0{
             
-            //add "$" before the money
-            
-//            let audAmountStr = (textField.text! as NSString).substringFromIndex(1)
-// self.AUDTextField.text = "\(audAmountStr!)"
-       
-    
             //display number
             
             let formatter = NSNumberFormatter()
             formatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
             formatter.currencyCode = "AUD"
-
-            let audAmountStr = formatter.numberFromString(textField.text!)//"A$1231231")
             
-            self.AUDTextField.text = "\(audAmountStr!)"
-            
+            if let audAmountStr = formatter.numberFromString(self.AUDTextField.text!){
+                self.AUDTextField.text = "\(audAmountStr)"
+            }
         }
     }
     
@@ -282,23 +248,18 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
         
         
         if textField.tag == audTextFieldTag && textField.text!.characters.count>0{
-            let audAmount:Float = (self.AUDTextField.text! as NSString).floatValue
             
-            //add "$" before the money
-//            let formatter = NSNumberFormatter()
-//            formatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
-//            self.AUDTextField.text = "$\(formatter.stringFromNumber(audAmount)!)"
-//
-
+            let audAmount:Float = (self.AUDTextField.text! as NSString).floatValue
             let formatter = NSNumberFormatter()
             formatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
             formatter.currencyCode = "AUD"
             //display converted amount
             self.AUDTextField.text = "\(formatter.stringFromNumber(audAmount)!)"
 
-            self.displayAmountBySelectedCurrency(audAmount)
 
             
+            
+            self.displayAmountBySelectedCurrency(audAmount)
             
         }else{
             self.amountDisplayLabel.text = ""
@@ -317,6 +278,10 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
         formatter.currencyCode = currencyCode
         //display converted amount
         self.amountDisplayLabel.text = "\(formatter.stringFromNumber(displayAmount)!)"
+        
+  
+        
+        
     }
 
 }
